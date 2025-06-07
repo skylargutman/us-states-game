3f6867f1568dfd93f43ec4cfcecfc195c4cfec1d
Scottie Gutman <Scottie@signaturecomputer.com>
2025-06-07 19:21:04 -0400

    complete game

--- main.py
+++ main.py
@@ -24,25 +24,26 @@ def print_label(state):
     writer.goto(x,y)
     writer.write(state)
 
-def get_mouse_click_coords(x,y):
-    print(x,y)
-
 game_is_on = True
 while game_is_on:
     answer_state = screen.textinput(title=f"{number_correct}/{number_guesses} States Correct", prompt="What's another state's name")
     number_guesses += 1
     if answer_state:
         answer_state = answer_state.title()
+        if answer_state in state_list and answer_state not in correct_answers:
+            print_label(answer_state)
+            number_correct += 1
+            correct_answers.append(answer_state)
+        else:
+            if answer_state == "Exit":
+                game_is_on = False
 
-    if answer_state in state_list and answer_state not in correct_answers:
-        print_label(answer_state)
-        number_correct += 1
-        correct_answers.append(answer_state)
-    else:
-        if answer_state.lower() == "exit":
-            game_is_on = False
+                #save list of states not guessed
+                not_guessed_states = []
+                for state in state_list:
+                    if not state in correct_answers:
+                        not_guessed_states.append(state)
+                pandas.DataFrame(not_guessed_states).to_csv("Not_guessed_states.csv")
 
-turtle.onscreenclick(get_mouse_click_coords)
 
-turtle.mainloop()
 
